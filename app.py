2ef26d851c89c4f78da1e6ec9e1385912f802ece
Jake Siney <jakesiney@gmail.com>
2023-08-22 12:55:43 +0100

    Containerised

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def get_home():
+    return "Sorry Jake, I'm afraid I cannot do that."
+
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        host="0.0.0.0"
+    )
